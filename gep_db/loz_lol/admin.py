8c09846f2f6711bc6a3d2d8a475ffc417bca7b50
Aristodemos <a.rpiko@gmail.com>
2014-08-13 13:47:48 +0300

    filtering part list

--- gep_db/loz_lol/admin.py
+++ gep_db/loz_lol/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from loz_lol.models import Aircraft, Part
+
+class PartAdmin(admin.ModelAdmin):
+	list_filter = ('part_description',)
+	list_display = ('part_description', 'part_serial', 'part_location')
+	'''
+	class Meta:
+		model = Part
+		ordering = ('part_description')
+	'''
+
+
+
 # Register your models here.
+admin.site.register(Aircraft)
+admin.site.register(Part, PartAdmin)
--- gep_db/loz_lol/models.py
+++ gep_db/loz_lol/models.py
@@ -10,17 +10,21 @@ class Aircraft(models.Model):
 	ac_flight_hours = models.CharField(max_length = 8)
 	ac_landings		= models.PositiveIntegerField()
 
+	def __str__(self):              # __unicode__ on Python 2
+		return str(self.ac_marks)
+
 class Part(models.Model):
+	part_description	= models.CharField(max_length = 30, null=True)
 	part_number			= models.CharField(max_length = 15)
 	part_serial			= models.CharField(max_length = 15)
 
-	part_ata_chapter	= models.CharFiel(max_length = 5)
+	part_ata_chapter	= models.CharField(max_length = 5)
 
 	PART_POSITION_CHOICES = (('1', 'Left'),('2', 'Right'), ('0', 'n/a'),)
 	part_position		= models.CharField(max_length=1, choices=PART_POSITION_CHOICES, default='0')
 
 	part_is_installed	= models.BooleanField()
-	part_location		= 
+	part_location		= models.CharField(max_length = 5)
 	#Flight Hours format 00:00 or 00.00
 	part_tot_flight_hours	= models.CharField(max_length = 8)
 	part_tot_landings		= models.PositiveIntegerField()
@@ -30,11 +34,15 @@ class Part(models.Model):
 	part_last_in_date		= models.DateField(auto_now=False, auto_now_add=False, null = True, blank = True)
 	part_last_rem_date		= models.DateField(auto_now=False, auto_now_add=False, null = True, blank = True)
 
-	def part_location_choices():
-		return {}
+	def __str__(self):              # __unicode__ on Python 2
+		return self.part_description
+
+	def filterOnDescription(self):
+		return Part.objects.values('part_description').distinct()
 
 	class Meta:
 		unique_together = (('part_number', 'part_serial'),)
+		#ordering = ('part_description',)
 
 
 #Forms
